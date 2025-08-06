e91a18dc9ba771c3fcbf51c0d8cc31843204df3a
Lucas Pecout <100285952+lucaspecout@users.noreply.github.com>
2025-08-06 21:38:35 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 # Initialisation de l'extension
 db = SQLAlchemy()
